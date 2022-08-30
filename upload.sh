e8b7f105e4eecd83c70f6f57e21541a7995aaf28
Matt Kaar <mkaar@cmu.edu>
2022-08-30 15:08:53 -0400

    Update publish URL

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ rsync -avP site/ mkaar@linux.andrew.cmu.edu:14-761/www/
 
 echo "Upload succeeded. Now visit this URL to publish the changes:"
 echo
-echo "https://www.andrew.cmu.edu/server/publish.html"
+echo "https://www.andrew.cmu.edu/cgi-bin/publish?FLAG=1&NAME=14-761"
